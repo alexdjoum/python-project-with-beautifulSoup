e801ec51fa72e65da960a39a758313e996745666
alexdjoum <kammegnealex@gmail.com>
2023-12-09 09:17:28 +0100

    handing connexion status and displaying number result for each search

--- pythprojv2.py
+++ pythprojv2.py
@@ -4,18 +4,25 @@ from bs4 import BeautifulSoup
 from tkinter import Tk, Label, Entry, Button, messagebox
 
 # Fonction pour effectuer le web scraping des résultats de recherche
-def web_scraping(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    print(soup)
-    # utiliser les méthodes de BeautifulSoup pour extraire les informations pertinentes des pages web
-    # ici je stoke le resultat dans un fichier.
+def web_scraping(url, label):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            # Utiliser les méthodes de BeautifulSoup pour extraire les informations pertinentes des pages web
+            # Ici, je stocke le résultat dans un fichier.
+            print(soup)
+        else:
+            label.config(text="Veuillez activer votre connexion", fg="red")
+    except requests.exceptions.RequestException as e:
+        label.config(text="Veuillez activer votre connexion", fg="red")
 
 # Fonction appelée lorsque le bouton "Ajouter au dictionnaire" est cliqué
 def ajouter_mot_cle():
     mot_cle = entry.get()  # Récupère le mot clé du champ d'entrée
     if mot_cle:
         dictionnaire.append(mot_cle)  # Ajoute le mot clé au dictionnaire
+        print("Le contenu du dictionnaire ==> ", dictionnaire)
         entry.delete(0, 'end')  # Efface le contenu du champ d'entrée
     else:
         messagebox.showwarning("Champ vide", "Veuillez entrer un mot clé.")
@@ -29,13 +36,33 @@ def soumettre_recherche():
     for mot_cle in dictionnaire:
         url = "https://www.google.com/search?q=" + mot_cle
 
+        # Créer un Label pour afficher le statut
+        label = Label(window, text="")
+        label.pack(pady= 10)
+
         # Effectue le web scraping des résultats de recherche
-        web_scraping(url)
+        web_scraping(url, label)
+
+        # Affiche le nombre de résultats de recherche dans le label
+        label.config(text=f"Nombre de résultats pour '{mot_cle}': {get_number_of_results(url)}")
+        label.configure(fg="green")
 
     messagebox.showinfo("Web scraping terminé", "Le web scraping des résultats de recherche est terminé.")
 
-    # Ouvre le fichier contenant les résultats
-    #webbrowser.open("resultats.txt")
+# Fonction pour obtenir le nombre de résultats de recherche pour un URL donné
+def get_number_of_results(url):
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"}
+    result = requests.get(url, headers=headers)    
+
+    soup = BeautifulSoup(result.content, 'html.parser')
+
+    total_results_text = soup.find("div", {"id": "result-stats"}) #.find(text=True, recursive=False) # this will give you the outer text which is like 'About 1,410,000,000 results'
+    results_num = total_results_text.text # now will clean it up and remove all the characters that are not a number .
+    return results_num 
+    print(results_num)
+
+    
+
 
 # Crée la fenêtre principale
 window = Tk()
@@ -57,3 +84,8 @@ dictionnaire = []
 
 # Lance la boucle principale de l'interface
 window.mainloop()
+
+
+import requests
+
+
